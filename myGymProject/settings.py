dfa38d2c896e67df39599af9f400d697f426244a
BIG CHIEF <64755630+chief-villager@users.noreply.github.com>
2023-03-18 17:48:55 +0000

    update

--- myGymProject/settings.py
+++ myGymProject/settings.py
@@ -132,7 +132,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'dollarjena28281992@gmail.com'
-EMAIL_HOST_PASSWORD = 'iupkczrckbrhftpf'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 
 
